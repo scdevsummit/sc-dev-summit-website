118b39162db9591194229a66f045529d4e470c88
Vinicius Dacal <viniciusldacal@gmail.com>
2016-03-14 21:11:44 -0300

    fixed lint issues

--- src/scripts/talks.js
+++ src/scripts/talks.js
@@ -1,20 +1,20 @@
-(function toggleSpeakerProfile(){
-  var speakers = document.getElementsByClassName('speaker-name');
+(function toggleSpeakerProfile () {
+  var speakers = document.getElementsByClassName('speaker-name')
 
   Array.prototype.forEach.call(speakers, function (speaker) {
-    speaker.addEventListener('mouseover', toggleProfile);
-    speaker.addEventListener('mouseleave', toggleProfile);
-    speaker.addEventListener('touchstart', toggleProfile);
-  });
+    speaker.addEventListener('mouseover', toggleProfile)
+    speaker.addEventListener('mouseleave', toggleProfile)
+    speaker.addEventListener('touchstart', toggleProfile)
+  })
 
-  function toggleProfile(event) {
-    var currentIndex, currentElement;
-    currentIndex = event.target.dataset.index;
-    currentElement = document.querySelector('[speaker="' + currentIndex + '"]');
+  function toggleProfile (event) {
+    var currentIndex, currentElement
+    currentIndex = event.target.dataset.index
+    currentElement = document.querySelector('[speaker="' + currentIndex + '"]')
 
     if ((currentElement.className.indexOf('active') >= 0) || (event.type === 'mouseleave')) {
-      return currentElement.classList.remove('active');
+      return currentElement.classList.remove('active')
     }
-    currentElement.classList.add('active');
+    currentElement.classList.add('active')
   }
-})();
+})()
