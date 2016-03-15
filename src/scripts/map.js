271549509d854ff96e55e76039550fcf890eaf84
Vinicius Dacal <viniciusldacal@gmail.com>
2016-03-15 00:36:19 -0300

    changed standard lint rules

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -1,11 +1,11 @@
-function initMap () {
+function initMap () { // eslint-disable-line no-unused-vars
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: -26.252893, lng: -48.850912},
     scrollwheel: false,
     zoom: 16
   })
 
-  new google.maps.Marker({
+  var marker = new google.maps.Marker({ // eslint-disable-line no-unused-vars
     position: {lat: -26.251873, lng: -48.855675},
     map: map,
     name: 'Univille'
